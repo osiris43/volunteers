25edb5183f6f72699e2ff3313d6adf65307dfa9d
Brett Bim <osiris43@gmail.com>
2016-07-10 17:19:04 -0500

    checking for table existence before dropping

--- db/migrate/20160527114007_drop_unnecessary_tables.rb
+++ db/migrate/20160527114007_drop_unnecessary_tables.rb
@@ -1,6 +1,6 @@
 class DropUnnecessaryTables < ActiveRecord::Migration
   def up
-    drop_table :organization_roles
-    drop_table :roles
+    drop_table :organization_roles if table_exists?(:organization_roles)
+    drop_table :roles if table_exists?(:roles)
   end
 end
